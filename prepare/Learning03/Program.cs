ed7b9733753c4178f023a2e42e81a9221bbdcb2e
itsyecenia <yecenia1130@byui.edu>
2023-05-17 20:43:26 -0600

    Finished Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // int number1 = 6;
+        // int number2 = 7;
+        // fraction fractionOne = new fraction();
+        // fraction fractionTwo = new fraction(number1);
+        // fraction fractionThree = new fraction(number1, number2);
+
+        // fractionOne.SetTop(6);
+        // Console.WriteLine(fractionOne.GetTop());
+
+        // fractionOne.SetBottom(7);
+        // Console.WriteLine(fractionOne.GetBottom());
+
+        fraction fractionOne = new fraction();
+        Console.WriteLine(fractionOne.GetFractionString());
+        Console.WriteLine(fractionOne.GetDecimalValue());
+
+        fractionOne.SetTop(5);
+        Console.WriteLine(fractionOne.GetFractionString());
+        Console.WriteLine(fractionOne.GetDecimalValue());
+
+        fraction fractionTwo = new fraction(3,4);
+        Console.WriteLine(fractionTwo.GetFractionString());
+        Console.WriteLine(fractionTwo.GetDecimalValue());
+
+        fractionTwo.SetBottom(3);
+        fractionTwo.SetTop(1);
+        Console.WriteLine(fractionTwo.GetFractionString());
+        Console.WriteLine(fractionTwo.GetDecimalValue());
     }
 }
---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,44 @@
+public class fraction{
+    private int _top;
+    private int _bottom;
+
+    public fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public fraction(int number){
+        _top = number;
+        _bottom = 1; 
+    }
+
+    public fraction(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop(){
+        return _top;
+    }
+
+    public void SetTop(int top){
+        _top = top;
+    }
+
+    public int GetBottom(){
+        return _bottom;
+    }
+
+     public void SetBottom(int bottom){
+        _bottom = bottom;
+    }
+
+    public string GetFractionString(){
+        return _top + "/" + _bottom;
+    }
+
+    public double GetDecimalValue(){
+        return (double)_top / (double)_bottom;
+    }
+
+}
